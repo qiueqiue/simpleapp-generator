2f66dcdddcfe23007911311247adac0654da4b17
qiueqiue <154318370+qiueqiue@users.noreply.github.com>
2023-12-19 17:29:27 +0800

    Update user.ts
    add user field data

--- src/buildinschemas/user.ts
+++ src/buildinschemas/user.ts
@@ -10,7 +10,23 @@ export const user:SchemaType ={
         uniqueKey:'email',        
         documentTitle:'fullname',
         requiredRoles:["SuperUser"],    
-        additionalAutoCompleteFields:['uid']      
+        additionalAutoCompleteFields:['uid'],
+        additionalApis:[{
+          action:"moredata",
+          entryPoint:"/moredata",
+          method:"post",
+          requiredRole:["Agent_update"],     
+          description:"get customer more"
+        },
+        {
+          action:"searchCertainValue",
+          entryPoint:"/searchCertainValue",
+          method:"post",
+          requiredRole:["Agent_update"],     
+          description:"get agent more"
+        }
+      
+      ]      
     },
     properties: {
         _id:{type:'string'},
@@ -26,6 +42,110 @@ export const user:SchemaType ={
         email: {type: "string",minLength:10,format: "email"},
         active: {type: "boolean",default:true},
         description: {type:"string"},
-        lastActivity: {type: "string",description:"capture ISO8601 last api call"}
+        lastActivity: {type: "string",description:"capture ISO8601 last api call"},
+        agentType: {
+          type: "object",
+          "x-foreignkey": "agenttype",
+          properties: {
+            _id: {
+              "type": "string"
+            },
+            code: {
+              "type": "string"
+            },
+            label: {
+              "type": "string"
+            }
+          }
+        },
+        agentIcno: {type:'string'},
+        agentDob: {
+          type: "string",
+          oneOf: [{ "format": "date" }, { "enum": [""] }]
+        },
+        agentTel1: {
+          type: "string",
+          minLength:9,
+          maxLength:17
+        },
+        agentTel2: {
+          type: "string"
+        },
+        agentGender: {
+          type: "string",
+          enum:[
+              "female",
+              "male"
+          ]
+        },
+        agentRace: {
+          type: "object",
+          "x-foreignkey": "race",
+          properties: {
+              _id: {
+                  type: "string"
+              },
+              code: {
+                  type: "string"
+              },
+              label: {
+                  type: "string"
+              }
+          }
+        },
+        agentColor: {
+          type: "string",
+          minLength:6,
+          maxLength:6
+        },
+        textColor: {
+          type: "string",
+          default:"000000"
+        },
+        groups: {
+          type: "array",
+          items: {
+            type: "object",
+            "x-foreignkey": "agentgroup",
+            properties: {
+              _id: {
+                type: "string"
+              },
+              code: {
+                type: "string"
+              },
+              label: {
+                type: "string"
+              }
+            }
+          }
+        },
+        clients: {
+          type: "array",
+          items: {
+            type: "object",
+            "x-foreignkey": "client",
+            properties: {
+              _id: {
+                type: "string"
+              },
+              code: {
+                type: "string"
+              },
+              label: {
+                type: "string"
+              }
+            }
+          }
+        },
+        opportunityCount:{
+          type:"number"
+        },
+        clientCount:{
+          type:"number"
+        },
+        activityCount:{
+          type:"number"
+        }
       }
 }
